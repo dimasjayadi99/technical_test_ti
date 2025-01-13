5d906f1b00465b49e1ca0f56f3d06e8c27115495
dimasjayadi99 <dimasjayadi7@gmail.com>
2025-01-14 01:02:54 +0700

    Update : Fullscreen video player

--- lib/app/features/course/presentation/pages/lesson_page.dart
+++ lib/app/features/course/presentation/pages/lesson_page.dart
@@ -6,6 +6,7 @@ import 'package:video_player/video_player.dart';
 import '../../../../core/consts/path.dart';
 import '../../../../core/dependency/injection_container.dart';
 import '../../../../core/shared/gap.dart';
+import 'package:chewie/chewie.dart';
 
 class LessonPage extends StatefulWidget {
   final String idLesson;
@@ -20,25 +21,43 @@ class LessonPage extends StatefulWidget {
 
 class _LessonPageState extends State<LessonPage> {
   VideoPlayerController? _controller;
-  bool _isPlaying = false;
+  ChewieController? _chewieController;
+  bool _isPlayerInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> _initializePlayer(String filePath) async {
+    _controller = VideoPlayerController.networkUrl(
+      Uri.parse(filePath),
+    );
+
+    try {
+      await _controller!.initialize();
+      _chewieController = ChewieController(
+        videoPlayerController: _controller!,
+        aspectRatio: _controller!.value.aspectRatio,
+        autoPlay: false,
+        looping: false,
+      );
+
+      setState(() {
+        _isPlayerInitialized = true;
+      });
+    } catch (e) {
+      setState(() {
+        _isPlayerInitialized = false;
+      });
+    }
+  }
 
   @override
   void dispose() {
+    _chewieController?.dispose();
     _controller?.dispose();
     super.dispose();
-  }
-
-  void _initializeVideo(String filePath) {
-    _controller = VideoPlayerController.networkUrl(Uri.parse(filePath))
-      ..initialize().then((_) {
-        setState(() {});
-        _controller?.play();
-      })
-      ..addListener(() {
-        setState(() {
-          _isPlaying = _controller?.value.isPlaying ?? false;
-        });
-      });
   }
 
   @override
@@ -82,141 +101,115 @@ class _LessonPageState extends State<LessonPage> {
           ),
         ),
         body: SafeArea(
-          child:
-              BlocBuilder<LessonBloc, LessonState>(builder: (context, state) {
-            if (state is LessonLoadingState) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (state is LessonFailedState) {
-              return Center(child: Text(state.message));
-            } else if (state is LessonSuccessState) {
-              final data = state.lesson;
-              if (_controller == null) {
-                _initializeVideo(Uri.encodeFull(data.filePath));
-              }
-              return Column(
-                children: [
-                  AspectRatio(
-                    aspectRatio: _controller!.value.aspectRatio,
-                    child: Stack(
-                      children: [
-                        VideoPlayer(_controller!),
-                        Positioned(
-                          bottom: 10,
-                          left: 10,
-                          right: 10,
-                          child: VideoProgressIndicator(
-                            _controller!,
-                            allowScrubbing: true,
-                            colors: const VideoProgressColors(
-                              playedColor: Colors.red,
-                              backgroundColor: Colors.grey,
-                            ),
-                          ),
-                        ),
-                        Center(
-                          child: IconButton(
-                            icon: Icon(
-                              _controller!.value.isPlaying
-                                  ? Icons.pause
-                                  : Icons.play_arrow,
-                              color: Colors.white,
-                              size: 50,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                if (_controller!.value.isPlaying) {
-                                  _controller?.pause();
-                                } else {
-                                  _controller?.play();
-                                }
-                              });
-                            },
-                          ),
-                        ),
-                      ],
+          child: BlocBuilder<LessonBloc, LessonState>(
+            builder: (context, state) {
+              if (state is LessonLoadingState) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (state is LessonFailedState) {
+                return Center(child: Text(state.message));
+              } else if (state is LessonSuccessState) {
+                final data = state.lesson;
+
+                if (!_isPlayerInitialized) {
+                  _initializePlayer(data.filePath);
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                return Column(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: _controller!.value.aspectRatio,
+                      child: Chewie(
+                        controller: _chewieController!,
+                      ),
                     ),
-                  ),
-                  const Gap.v(h: 16),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          "Lesson 1",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const Gap.v(h: 8),
-                        Text(
-                          widget.titleLesson,
-                          style: const TextStyle(fontSize: 18),
-                        ),
-                        const Gap.v(h: 8),
-                        Row(
-                          children: [
-                            const Text(
-                              "Created by Diego Davila",
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                            const Spacer(),
-                            ClipOval(
-                              child: Image.asset(
-                                PathConst.defaultFlag,
-                                width: 24,
-                                height: 24,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            const Gap.h(w: 8),
-                            Text(
-                              'in English',
-                              style: Theme.of(context).textTheme.labelMedium,
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  const Gap.v(h: 16),
-                  Expanded(
-                    child: Container(
-                      color: Colors.grey[850],
-                      child: ListView(
+                    const Gap.v(h: 16),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          _buildContentCard(
+                          const Text(
                             "Lesson 1",
-                            "Welcome to 23 Ways of Earning Money with AI",
-                            "02:30",
-                            'https://www.worldtesolacademy.com/wp-content/uploads/online-lessons-banner.jpg',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                          _buildContentCard(
-                            "PDF",
-                            "The ChatGPT Prompts Guide",
-                            "15:00",
-                            'https://www.worldtesolacademy.com/wp-content/uploads/online-lessons-banner.jpg',
+                          const Gap.v(h: 8),
+                          Text(
+                            widget.titleLesson,
+                            style: const TextStyle(fontSize: 18),
                           ),
-                          _buildContentCard(
-                            "Lesson 2",
-                            "Get Started with ChatGPT",
-                            "18:00",
-                            'https://www.worldtesolacademy.com/wp-content/uploads/online-lessons-banner.jpg',
+                          const Gap.v(h: 8),
+                          Row(
+                            children: [
+                              const Text(
+                                "Created by Diego Davila",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                              const Spacer(),
+                              ClipOval(
+                                child: Image.asset(
+                                  PathConst.defaultFlag,
+                                  width: 24,
+                                  height: 24,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              const Gap.h(w: 8),
+                              Text(
+                                'in English',
+                                style: Theme.of(context).textTheme.labelMedium,
+                              )
+                            ],
                           ),
                         ],
                       ),
                     ),
+                    const Gap.v(h: 16),
+                    Expanded(
+                      child: Container(
+                        color: Colors.grey[850],
+                        child: ListView(
+                          children: [
+                            _buildContentCard(
+                              "Lesson 1",
+                              "Welcome to 23 Ways of Earning Money with AI",
+                              "02:30",
+                              'https://www.worldtesolacademy.com/wp-content/uploads/online-lessons-banner.jpg',
+                            ),
+                            _buildContentCard(
+                              "PDF",
+                              "The ChatGPT Prompts Guide",
+                              "15:00",
+                              'https://www.worldtesolacademy.com/wp-content/uploads/online-lessons-banner.jpg',
+                            ),
+                            _buildContentCard(
+                              "Lesson 2",
+                              "Get Started with ChatGPT",
+                              "18:00",
+                              'https://www.worldtesolacademy.com/wp-content/uploads/online-lessons-banner.jpg',
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              } else {
+                return const Center(
+                  child: Text(
+                    'Terjadi kesalahan saat menampilkan halaman',
                   ),
-                ],
-              );
-            } else {
-              return const Center(
-                  child: Text('Terjadi kesalahan saat menampilkan halaman'));
-            }
-          }),
+                );
+              }
+            },
+          ),
         ),
       ),
     );
